14b8052cd83dd7dff69dcca6cdd206016142d702
Harsha A M <harsha.am@ymedialabs.com>
2023-09-12 23:09:56 +0530

    Updated build static files

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://sample-testing.azurewebsites.net/check")
+      .then((res) => setUsers(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{users}</h1>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use(express.static("./client/build"));
 
 app.get("/check", (req, res) => {
   res.send({
@@ -9,6 +11,11 @@ app.get("/check", (req, res) => {
   })
 })
 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
+
 app.listen(3007, () => {
   console.log("Server is running.");
 })
+
