0fa6f2389ddf8a7f28850e9b60fe084c86bbdc5d
Harsha A M <harsha.am@ymedialabs.com>
2023-09-13 11:32:52 +0530

    Restructured files

---
+++ server/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static("./client/build"));
+
+app.get("/check", (req, res) => {
+  res.send({
+    message: "Lekim your server is working"
+  })
+})
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
+
+app.listen(3007, () => {
+  console.log("Server is running.");
+})
+
