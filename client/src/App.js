ce3918387ce9eebbdebd99371cc7a1eca4b353d5
Harsha A M <harsha.am@ymedialabs.com>
2023-09-13 17:34:28 +0530

    Build file updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,6 @@
-import { useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import { useState } from "react";
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://testing-azure.onrender.com/")
-      .then((res) => setUsers(res.data))
-      .catch((err) => console.log(err));
-  }, []);
-
 
   return (
     <div className="App">
