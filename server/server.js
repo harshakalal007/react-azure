cb89ec944d53b1e9d995f07b2ae044cd10a4c70d
Harsha A M <harsha.am@ymedialabs.com>
2023-09-13 17:19:01 +0530

    testing in azure

--- server/server.js
+++ server/server.js
@@ -2,24 +2,25 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+require("dotenv").config();
+const port = process.env.PORT ; 
 
 app.use(express.static("./client/build"));
 
 app.get("/", (req, res) => {
-  res.send({message: "Hey, you are using render to deploy."})
-})
+  res.send({ message: "Hey, you are using render to deploy." });
+});
 
 app.get("/check", (req, res) => {
   res.send({
     message: "Lekim your server is working"
-  })
-})
+  });
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-})
+});
 
-app.listen(3007, () => {
-  console.log("Server is running.");
-})
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});
